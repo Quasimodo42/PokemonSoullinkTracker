ae72870a858b6f672388b7ebb4fb090d9f6f3fde
Luis Bernardo <Luisbedo20@gmail.com>
2026-06-18 17:03:54 +0200

    test: winsparkle init test

--- PokemonSoullinkTracker/main.cpp
+++ PokemonSoullinkTracker/main.cpp
@@ -2,10 +2,29 @@
 
 #include <QApplication>
 
+#ifdef Q_OS_WIN
+#include "winsparkle.h"
+#endif
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+#ifdef Q_OS_WIN
+    win_sparkle_set_appcast_url("https://quasimodo42.github.io/PokemonSoullinkTracker/appcast.xml");
+
+    win_sparkle_set_app_details(L"Quasimodo42", L"PokemonSoullinkTracker", L"0.1");
+
+    win_sparkle_init();
+#endif
+
+
     MainWindow w;
     w.show();
+
+#ifdef Q_OS_WIN
+    win_sparkle_cleanup();
+#endif
+
     return QApplication::exec();
 }
